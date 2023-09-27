06026f7953adaba705b4e24c59a1ca4a850055e4
Maga Bashirov <maga.bashirov@gmail.com>
2023-09-27 15:29:49 +0300

    chore: eslient added to webpack dev config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,8 +1,11 @@
 const { merge } = require("webpack-merge");
+const ESLintPlugin = require("eslint-webpack-plugin");
 const commonConfig = require("./webpack.config.common");
+
 module.exports = merge(commonConfig, {
   mode: "development",
   devtool: "inline-source-map",
+  plugins: [new ESLintPlugin()],
   devServer: {
     port: 3000,
     hot: true,
