6ae8e3f31d7774b95fdca3850624bde199d61664
Maga Bashirov <maga.bashirov@gmail.com>
2023-09-27 10:47:12 +0300

    chore: added webpack dev server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,5 +2,10 @@ const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.config.common");
 module.exports = merge(commonConfig, {
   mode: "development",
-  devtool: "inline-source-map"
+  devtool: "inline-source-map",
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true
+  }
 });
